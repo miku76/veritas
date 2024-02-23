c7c5afdad37648d1fec7a7f22bdfba622d7f03dc
veritas-sot <veritas-sot@proton.me>
2024-02-23 15:07:57 +0100

    updater is no singleton

--- src/veritas/sot/updater.py
+++ src/veritas/sot/updater.py
@@ -1,55 +1,67 @@
-from pynautobot import api
 from loguru import logger
 from veritas.tools import tools
 
 
 class Updater(object):
+    """Generic class to update any properties in nautobot.
 
-    def __new__(cls, sot):
-        cls._instance = None
-        cls._sot = sot
-        cls._nautobot = cls._sot.open_nautobot()
-        cls._endpoints = {'sites': cls._nautobot.dcim.sites,
-                          'manufacturers': cls._nautobot.dcim.manufacturers,
-                          'platforms': cls._nautobot.dcim.platforms,
-                          'devices': cls._nautobot.dcim.devices,
-                          'device_roles': cls._nautobot.dcim.device_roles,
-                          'prefixes': cls._nautobot.ipam.prefixes,
-                          'location_types': cls._nautobot.dcim.location_types,
-                          'locations': cls._nautobot.dcim.locations,
-                          'interface_templates': cls._nautobot.dcim.interface_templates,
-                          'tags': cls._nautobot.extras.tags,
-                          'custom_fields': cls._nautobot.extras.custom_fields,
-                          'custom_field_choices': cls._nautobot.extras.custom_field_choices,
-                          'webhooks': cls._nautobot.extras.webhooks,
-                          'device_types': cls._nautobot.dcim.device_types,
-                          'console_port_templates': cls._nautobot.dcim.console_port_templates,
-                          'power_port_templates': cls._nautobot.dcim.power_port_templates,
-                          'device_bay_templates': cls._nautobot.dcim.device_bay_templates, }
-                
-        # singleton
-        if cls._instance is None:
-            cls._instance = super(Updater, cls).__new__(cls)
-        return cls._instance
+    Parameters
+    ----------
+    sot : Sot
+        the sot object to use
+    """
+    def __init__(self, sot):
+        self._instance = None
+        self._sot = sot
+        self._nautobot = self._sot.open_nautobot()
+        self._endpoints = {'sites': self._nautobot.dcim.sites,
+                           'manufacturers': self._nautobot.dcim.manufacturers,
+                           'platforms': self._nautobot.dcim.platforms,
+                           'devices': self._nautobot.dcim.devices,
+                           'device_roles': self._nautobot.dcim.device_roles,
+                           'prefixes': self._nautobot.ipam.prefixes,
+                           'location_types': self._nautobot.dcim.location_types,
+                           'locations': self._nautobot.dcim.locations,
+                           'interface_templates': self._nautobot.dcim.interface_templates,
+                           'tags': self._nautobot.extras.tags,
+                           'custom_fields': self._nautobot.extras.custom_fields,
+                           'custom_field_choices': self._nautobot.extras.custom_field_choices,
+                           'webhooks': self._nautobot.extras.webhooks,
+                           'device_types': self._nautobot.dcim.device_types,
+                           'console_port_templates': self._nautobot.dcim.console_port_templates,
+                           'power_port_templates': self._nautobot.dcim.power_port_templates,
+                           'device_bay_templates': self._nautobot.dcim.device_bay_templates, }
 
-    def update_entity(self, func, properties, getter):
+    def update(self, endpoint:str, values:dict, getter:dict):
+        """update entity in sot
+
+        Parameters
+        ----------
+        endpoint : str
+            name of the endpoint to use
+        values : dict
+            values to update
+        getter : dict
+            getter to use to check if entity is already in sot
+
+        Returns
+        -------
+        entity : object
+            the entity that was updated
         """
-        func: used to get the updates entity
-        properties: the new properties of the entity
-        """
-
-        # check if entity is part of sot
+        # check if entity is already in sot
         try:
-            entity = func.get(**getter)
+            endpoint_func = self._endpoints.get(endpoint, None)
+            entity = endpoint_func.get(**getter)
             if entity is None:
-                logger.debug(f'entity not found in sot')
+                logger.debug('entity not found in sot')
                 return None
         except Exception as exc:
             logger.error(f'could not get entity; got exception {exc}')
             return None
 
         try:
-            success = entity.update(properties)
+            success = entity.update(values)
             if success:
                 logger.debug("entity updated in sot")
             else:
@@ -61,19 +73,8 @@ class Updater(object):
 
         return entity
 
-    def update(self, *unnamed, **named):
-        properties = tools.convert_arguments_to_properties(*unnamed, **named)
-        endpoint_name = properties.get('endpoint')
-        values = properties.get('values')
-        getter = properties.get('getter')
-        if endpoint_name is None or values is None or getter is None:
-            logger.error('endpoint, getter, and values must be set')
-            return None
-        endpoint = self._endpoints.get(endpoint_name)
-        return self.update_entity(endpoint, values, getter)
-
     def update_by_id(self, *unnamed, **named):
         properties = tools.convert_arguments_to_properties(*unnamed, **named)
         id = properties.get('id')
         del properties['id']
-        d = self._nautobot.dcim.devices.update(id=id, data=properties)
+        return self._nautobot.dcim.devices.update(id=id, data=properties)
