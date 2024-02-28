7d435b67300fddedf6982eee4ebf0f386ccb8f9b
veritas-sot <veritas-sot@proton.me>
2024-02-28 19:11:50 +0100

    more documentation
    more documentation

--- src/veritas/devicemanagement/abstract_devicemanagement.py
+++ src/veritas/devicemanagement/abstract_devicemanagement.py
@@ -1,25 +1,52 @@
 from abc import ABC, abstractmethod
 
 class AbstractDeviceManagement(ABC):
-    # see also veritas/src/veritas/devicemanagement/scrapli.py
+    """abstract class for device management
+
+    Parameters
+    ----------
+    ip : str
+        ip address of the device
+    username : str
+        username for the device
+    password : str
+        password for the device
+    platform : str
+        platform of the device
+    ssh_keyfile : str
+        ssh keyfile for the device
+    port : int
+        port for the device
+    manufacturer : str
+        manufacturer of the device
+    scrapli_loglevel : str
+        loglevel for scrapli
+    
+     # see also veritas/src/veritas/devicemanagement/scrapli.py
+    """    
     
     @abstractmethod
-    def __init__(self, ip, username, password, platform='ios', ssh_keyfile=None, port=22, manufacturer='cisco', scrapli_loglevel='none'):
+    def __init__(self, ip:str, username:str, password:str, platform:str='ios', 
+                 ssh_keyfile:str=None, port:int=22, manufacturer:str='cisco', scrapli_loglevel:str='none'):
         pass
 
     @abstractmethod
     def open(self):
+        """open connection to the device"""
         pass
 
     @abstractmethod
     def close(self):
+        """close connection to the device"""
         pass
 
     @abstractmethod
     def get_facts(self):
+        """get facts from the device"""
         pass
 
     @abstractmethod
     def get_config(self, source):
+        """get config from the device"""
         pass
 
